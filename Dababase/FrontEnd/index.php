a18d52bb2f79c1ec1bf32603ff8e25eeead1be9f
Abduboulaich37 <abduboulaich37@gmail.com>
2022-10-14 07:24:29 +0100

    Edit Folders

---
+++ Dababase/FrontEnd/index.php
@@ -0,0 +1,44 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <!-- Title -->
+    <title>Brief 1 : Crud Promotion using PHP POO</title>
+</head>
+<body><br>
+    <!-- Name of Crud -->
+    <h1 class="text-center text-info">YourTasks</h1>
+    <div class="container">
+        <form action="index.php" method="post">
+            <div class="form-group">
+                <label>Name</label>
+                <input type="text" name="name" placeholder="Enter name of promotion" class="form-control">
+            </div>
+            <div class="form-group text-center">
+                <input type="submit" name="submit" value="Submit" class="btn btn-info">
+            </div>
+        </form><br>
+        <h3 class="text-center text-danger">Display Promotions</h3>
+        <table class="table table-bordered">
+            <tr class="bg-primary text center">
+                <th>ID</th>
+                <th>Name</th>
+            </tr>
+
+        </table>
+
+
+    </div>
+</body>
+</html>
